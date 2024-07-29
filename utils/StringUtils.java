5dec27424c8709cf942c4af5c9b4963d8084cf2c
Prajeet Man Dhoubhadel <prajeet.dhoubhadel19@gmail.com>
2024-07-29 13:56:10 +0545

    Initial commit

---
+++ utils/StringUtils.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Petpulse.utils;
+
+/**
+ *
+ * @author praje
+ */
+public class StringUtils {
+    public static final String INTEGER_MESSAGE = "Please enter the value in integer format!! ";
+    public static final String STRING_MESSAGE = "Please enter the value in String format!! ";
+    
+}
